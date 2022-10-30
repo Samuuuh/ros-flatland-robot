69b070ab8624b96d1f096903d70453b03a032681
Diogo Samuel Fernandes <45127751+Samuuuh@users.noreply.github.com>
2022-10-30 18:37:39 +0000

    Update run.py

--- assignment_one/run.py
+++ assignment_one/run.py
@@ -3,14 +3,14 @@ import os
 from threading import Thread
 from time import sleep
 
-from world import WORLD, MAP
+from world import GenerateMap
 from robot_movement import main
 
 if __name__ == "__main__":
     with open("models/map.yaml", "w") as map_file:
-        map_file.write(MAP)
+        map_file.write(GenerateMap.get_map())
 
     with open("models/world.yaml", "w") as world_file:
-        world_file.write(WORLD)
+        world_file.write(GenerateMap.get_world())
 
     os.system("cd ../.. && roslaunch assignment_one assignment_one.launch")
